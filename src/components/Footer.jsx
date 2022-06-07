97e4fa3f20011188db303b498409352bef07a5f4
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-06-07 19:23:04 +0530

    created Footer and newsletter components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import styled from '@emotion/styled';
+
+// Material Ui inports 
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
+
+const Container = styled.div`
+    display: flex;
+`
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`
+const Logo = styled.h1`
+
+`
+const Desc = styled.p`
+    margin: 20px 0;
+`
+const SocialContainer = styled.h1`
+    display: flex;
+`
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+    color: white;
+    background-color: #${props => props.color};
+
+`
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`
+const Payment = styled.img`
+    width: 50%;
+`
+
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>P-Mart.</Logo>
+                <Desc>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias expedita accusantium libero voluptatem ad minima totam minus dicta, dolorum distinctio atque. Deleniti repellendus quo, dicta illo placeat quisquam facere ea!</Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <FacebookIcon />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <InstagramIcon />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <TwitterIcon />
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <PinterestIcon />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wish List</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem> <LocationOnIcon style={{marginRight: "10px"}} /> 123 SV Road, South Mumbai - 40005 </ContactItem>
+                <ContactItem> <PhoneIcon style={{marginRight: "10px"}} /> +91 234 123 4245 </ContactItem>
+                <ContactItem> <MailOutlinedIcon style={{marginRight: "10px"}}/> contact@pmart.com </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+
+        </Container>
+    )
+}
+
+export default Footer
